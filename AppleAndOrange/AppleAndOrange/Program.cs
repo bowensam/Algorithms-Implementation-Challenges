626a80a0810b39c68375d457f75cc27628cfdf9b
Bowen <Bowen@BOWEN-PC>
2017-12-03 02:01:14 -0500

    2nd Dec (Sat)

---
+++ AppleAndOrange/AppleAndOrange/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppleAndOrange
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string[] tokens_s = Console.ReadLine().Split(' ');
+            int s = Convert.ToInt32(tokens_s[0]);
+            int t = Convert.ToInt32(tokens_s[1]);
+            string[] tokens_a = Console.ReadLine().Split(' ');
+            int a = Convert.ToInt32(tokens_a[0]);
+            int b = Convert.ToInt32(tokens_a[1]);
+            string[] tokens_m = Console.ReadLine().Split(' ');
+            int m = Convert.ToInt32(tokens_m[0]);
+            int n = Convert.ToInt32(tokens_m[1]);
+            string[] apple_temp = Console.ReadLine().Split(' ');
+            int[] apple = Array.ConvertAll(apple_temp, Int32.Parse);
+            string[] orange_temp = Console.ReadLine().Split(' ');
+            int[] orange = Array.ConvertAll(orange_temp, Int32.Parse);
+
+            int applesHit = 0;
+            for (int i = 0; i < apple.Length; i++)
+            {
+                if (((a + apple[i]) >= s) && ((a + apple[i]) <= t))
+                    applesHit += 1;
+            }
+
+            int orangesHit = 0;
+            for (int i = 0; i < orange.Length; i++)
+            {
+                if (((b + orange[i]) >= s) && ((b + orange[i]) <= t))
+                    orangesHit += 1;
+            }
+
+            Console.WriteLine(applesHit);
+            Console.WriteLine(orangesHit);
+        }
+    }
+}
---
+++ BonAppetit/BonAppetit/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonAppetit
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string[] tokens_n = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens_n[0]);
+            int k = Convert.ToInt32(tokens_n[1]);
+            string[] ar_temp = Console.ReadLine().Split(' ');
+            int[] ar = Array.ConvertAll(ar_temp, Int32.Parse);
+            int b = Convert.ToInt32(Console.ReadLine());
+            String result = BonAppetit(n, k, b, ar);
+            Console.WriteLine(result);
+        }
+
+        static String BonAppetit(int n, int k, int b, int[] ar)
+        {
+            // Complete this function
+            int total = 0;
+            for (int dish = 0; dish < ar.Length; dish++)
+                total += ar[dish];
+
+            int b_actual = (total - ar[k]) / 2;
+
+            if (b_actual - b == 0)
+                return "Bon Appetit";
+            else
+                return (Math.Abs(b_actual - b)).ToString();
+        }
+    }
+}
